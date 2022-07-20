fbbc407ad8d491c8f45192235a86da2813e1f09a
Adam Alderton <adam.alderton@yahoo.co.uk>
2022-07-20 14:38:42 +0100

    two plots made nicely

--- src/simple_case_integration.py
+++ src/simple_case_integration.py
@@ -1,21 +1,35 @@
-# TODO: Comment documentation!
-# Define what gamma is
+# A prototpye for the numerical integration of the binary sliced Gaussian
+# For more information, please see the full report attached to the project.
+# This should, in general, be interfaced to by an external program.
+#
+# Gamma: Integral corresponding to bits that, subject to noise, have been measured to reside at the other side of the guard band.
 # Tau: 1 - throughput integral. Integral of pdf inside guard band, such that throughput is 1 - tau.
+
+######################################################
+
+# CAVEAT: This only analyses 'half' of the X gaussian, hence gamma is not actually a probability
+# as only 0.5 of X probability is considered. However, considering that this system is symmetric,
+# the extension of the analysis to the right hand side of the Gaussian which would mean that gamma
+# would double as there is a 'gamma' either side of the guard band. Therefore, the BER is actually the
+# BER for the entire symmetrical system as the factor of half for total gamma and fraction of 
+# distribution analysed cancel: (0.5 dist) / (0.5 gamma) = dist / gamma.
+
+######################################################
 
 from typing import Tuple
 import numpy as np
 from scipy.integrate import dblquad
 
-def _gamma_integral(Y, X, sigma_X, sigma_nu, mu_X):
+def _gamma_integral(Y, X, sigma_X, sigma_eta, mu_X):
     
     var_X = sigma_X*sigma_X
-    var_nu = sigma_nu*sigma_nu
+    var_nu = sigma_eta*sigma_eta
     
     XmmuX = X - mu_X
     YmX = Y - X
     
     return \
-        (1.0 / (2*np.pi * sigma_X * sigma_nu) ) * \
+        (1.0 / (2*np.pi * sigma_X * sigma_eta) ) * \
         np.exp(- 0.5 * (
                 ( (XmmuX*XmmuX) / (var_X) ) + \
                 ( (YmX*YmX) / (var_nu) )
@@ -24,26 +38,26 @@ def _gamma_integral(Y, X, sigma_X, sigma_nu, mu_X):
 
 
 # Numerically evaluate the gamme integral as per the OneNote page. 
-def evaluate_gamma(sigma_X, sigma_nu, mu_X, beta) -> Tuple[float, float]:
+def evaluate_gamma(sigma_X, sigma_eta, mu_X, beta) -> Tuple[float, float]:
     return dblquad(
         _gamma_integral,                        # Function to integrate
         -np.inf,                                # Lower bound of X
-        mu_X - beta,                            # Upper bound of X
+        mu_X,                                   # Upper bound of X
         mu_X + beta,                            # Lower bound of Y
         np.inf,                                 # Upper bound of Y
-        args = (sigma_X, sigma_nu, mu_X)        # Extra arguments
+        args = (sigma_X, sigma_eta, mu_X)       # Extra arguments
     )
 
 # Essentially the same at the gamme integral, except for the fact that different integral limits are given.
 # See the OneNote page for more information.
-def evaluate_tau(sigma_X, sigma_nu, mu_X, beta) -> Tuple[float, float]:
+def evaluate_tau(sigma_X, sigma_eta, mu_X, beta) -> Tuple[float, float]:
     return dblquad(
         _gamma_integral,
-        -np.inf,
-        mu_X - beta,
-        mu_X - beta,
-        mu_X + beta,
-        args = (sigma_X, sigma_nu, mu_X)
+        -np.inf,                                # Lower bound of X
+        mu_X,                                   # Upper bound of X
+        mu_X - beta,                            # Lower bound of Y
+        mu_X + beta,                            # Upper bound of Y
+        args = (sigma_X, sigma_eta, mu_X)       # Extra arguments
     )
 
 
@@ -61,15 +75,14 @@ if __name__ == "__main__":
     
     ############
     
-    
     print("\n\t# X_var = {:.3f}.\n\t# noise_var = {:.3f}.\n\t# guard_band_width = {:.3f}.".format(X_var, noise_var, band_width))
     print("\n\t# Evaluating ...")
     
     gamma, gamma_err = evaluate_gamma(np.sqrt(X_var), np.sqrt(noise_var), mu_X, band_width / 2.0)
     
-    phi, phi_err = evaluate_tau(np.sqrt(X_var), np.sqrt(noise_var), mu_X, band_width / 2.0)
+    tau, tau_err = evaluate_tau(np.sqrt(X_var), np.sqrt(noise_var), mu_X, band_width / 2.0)
     
-    throughput = 1.0 - phi
+    throughput = 1.0 - tau
     
     print("\n\tQBER = {:.4f}% with numerical integration error = {:.2e}%.".format(gamma * 100.0, gamma_err * 100.0))
-    print("\tTPut = {:.4f}% with numerical integration error = {:.2e}%.".format(throughput * 100.0, phi_err * 100.0))
+    print("\tTPut = {:.4f}% with numerical integration error = {:.2e}%.".format(throughput * 100.0, tau_err * 100.0))
---
+++ src/two_var_analysis.py
@@ -0,0 +1,93 @@
+from configparser import Interpolation
+import numpy as np
+import matplotlib.pyplot as plt
+from one_var_analysis import QBER_FILENAME
+
+# Import appropriate integration functions
+from simple_case_integration import evaluate_gamma
+from simple_case_integration import evaluate_tau
+
+##################################################
+
+# Adjustable parameters
+BETA_MIN = 0.0
+BETA_MAX = 1.4
+NUM_BETAS = 20
+NOISE_MIN = 0.01 # Don't divide by zero
+NOISE_MAX = 2.0
+NUM_NOISES = 20
+X_VAR = 1.0
+
+QBER_FILENAME = "results/two_var_simple_QBER.png"
+TAU_FILENAME = "results/two_var_simple_TAU.png"
+IMG_SIZE = (7, 5)
+DPI = 300
+FONTSIZE = 16
+QBER_CONTOUR_LEVELS = (5, 10, 15)
+TAU_CONTOUR_LEVELS = (10, 20, 30)
+
+#################
+
+sigma_etas = np.linspace(NOISE_MIN, NOISE_MAX, NUM_NOISES) # Equivalent to sigma_nu in the corresponding notes
+betas = np.linspace(BETA_MIN, BETA_MAX, NUM_BETAS)
+sigma_X = np.sqrt(X_VAR)
+mu_X = 0.0
+
+#################
+
+QBERs = np.empty((NUM_BETAS, NUM_NOISES))
+taus = np.empty(QBERs.shape)
+
+for i, beta in enumerate(betas):
+    for j, sigma_eta, in enumerate(sigma_etas):
+        
+        if (j == 0):
+            print("# {} / {} combinations processed.".format(i * NUM_BETAS, NUM_BETAS * NUM_NOISES))
+        
+        # Evaluate gamma then convert it to a percentage by multiplying by 100
+        QBERs[i][j] = evaluate_gamma(sigma_X, sigma_eta, mu_X, beta)[0] * 100.0
+        
+        # Do the same with tau (= 1 - throughput)
+        taus[i][j] = evaluate_tau(sigma_X, sigma_eta, mu_X, beta)[0] * 100.0
+        
+#################
+
+print("# Producing images ...")
+
+############## PRODUCE QBER IMAGE ###############
+
+fig, ax = plt.subplots(1, figsize = IMG_SIZE)
+
+img = ax.imshow(QBERs, cmap = "gist_gray", interpolation = "bicubic", origin = "lower", aspect = "auto", extent = (NOISE_MIN, NOISE_MAX, BETA_MIN, BETA_MAX))
+cnt = ax.contour(QBERs, levels = QBER_CONTOUR_LEVELS, colors = ("yellow", "blue", "red"), extent = (NOISE_MIN, NOISE_MAX, BETA_MIN, BETA_MAX))
+
+ax.set_xlabel("$\\sigma_\\eta$", fontsize = FONTSIZE)
+ax.set_ylabel("$\\beta$", fontsize = FONTSIZE)
+
+cbar = plt.colorbar(img, ax = ax)
+cbar.set_label("QBER %", fontsize = FONTSIZE)
+cbar.add_lines(cnt)
+cbar.minorticks_on()
+
+plt.tight_layout()
+
+plt.savefig(QBER_FILENAME, dpi = DPI)
+
+############## PRODUCE THROUGHPUT IMAGE ###############
+
+fig, ax = plt.subplots(1, figsize = IMG_SIZE)
+
+img = ax.imshow(taus, cmap = "gist_gray", interpolation = "bicubic", origin = "lower", aspect = "auto", extent = (NOISE_MIN, NOISE_MAX, BETA_MIN, BETA_MAX))
+cnt = ax.contour(taus, levels = TAU_CONTOUR_LEVELS, colors = ("yellow", "blue", "red"), extent = (NOISE_MIN, NOISE_MAX, BETA_MIN, BETA_MAX))
+
+ax.set_xlabel("$\\sigma_\\eta$", fontsize = FONTSIZE)
+ax.set_ylabel("$\\beta$", fontsize = FONTSIZE)
+
+cbar = plt.colorbar(img, ax = ax)
+cbar.set_label("$\\tau$ %", fontsize = FONTSIZE)
+cbar.add_lines(cnt)
+cbar.minorticks_on()
+
+plt.tight_layout()
+
+plt.savefig(TAU_FILENAME, dpi = DPI)
